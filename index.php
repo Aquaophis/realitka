54e39404daa2b0072592c85038705c3ecbfeb0d3
Aquaophis <46408429+Aquaophis@users.noreply.github.com>
2019-01-07 13:01:57 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,413 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+	<?php include 'includes\title.html';?>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/ionicons.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.timepicker.css">
+
+    
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <div class="top">
+    	<div class="container">
+    		<div class="row d-flex align-items-center">    			
+    		</div>
+    	</div>
+    </div>
+
+	<?php include 'includes\navbar.html';?>
+    <!-- END nav -->
+
+    <section class="home-slider owl-carousel">
+      <div class="slider-item" style="background-image:url(images/bg_1.jpg);">
+      	<div class="overlay"></div>
+        <div class="container">
+          <div class="row no-gutters slider-text align-items-md-end align-items-center justify-content-end">
+          <div class="col-md-6 text p-4 ftco-animate">
+            <h1 class="mb-3">Florida 5, Pinecrest, FL</h1>
+            <span class="location d-block mb-3"><i class="icon-my_location"></i> Melbourne, Vic 3004</span>
+            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+            <span class="price">$28,000</span>
+            <a href="#" class="btn-custom p-3 px-4 bg-primary">View Details <span class="icon-plus ml-1"></span></a>
+          </div>
+        </div>
+        </div>
+      </div>
+
+      <div class="slider-item" style="background-image:url(images/bg_2.jpg);">
+      	<div class="overlay"></div>
+        <div class="container">
+          <div class="row no-gutters slider-text align-items-md-end align-items-center justify-content-end">
+          <div class="col-md-6 text p-4 ftco-animate">
+            <h1 class="mb-3">3015 Grand Avenue, CocoWalk</h1>
+            <span class="location d-block mb-3"><i class="icon-my_location"></i> Melbourne, Vic 3004</span>
+            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+            <span class="price">$28,000</span>
+            <a href="#" class="btn-custom p-3 px-4 bg-primary">View Details <span class="icon-plus ml-1"></span></a>
+          </div>
+        </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="ftco-section bg-light">
+      <div class="container">
+        <div class="row d-flex">
+          <div class="col-md-3 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services py-4 d-block text-center">
+              <div class="d-flex justify-content-center"><div class="icon"><span class="flaticon-pin"></span></div></div>
+              <div class="media-body p-2 mt-2">
+                <h3 class="heading mb-3">Find Places Anywhere In The World</h3>
+                <p>A small river named Duden flows by their place and supplies.</p>
+              </div>
+            </div>      
+          </div>
+          <div class="col-md-3 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services py-4 d-block text-center">
+              <div class="d-flex justify-content-center"><div class="icon"><span class="flaticon-detective"></span></div></div>
+              <div class="media-body p-2 mt-2">
+                <h3 class="heading mb-3">We Have Agents With Experience</h3>
+                <p>A small river named Duden flows by their place and supplies.</p>
+              </div>
+            </div>    
+          </div>
+          <div class="col-md-3 d-flex align-sel Searchf-stretch ftco-animate">
+            <div class="media block-6 services py-4 d-block text-center">
+              <div class="d-flex justify-content-center"><div class="icon"><span class="flaticon-house"></span></div></div>
+              <div class="media-body p-2 mt-2">
+                <h3 class="heading mb-3">Buy &amp; Rent Modern Properties</h3>
+                <p>A small river named Duden flows by their place and supplies.</p>
+              </div>
+            </div>      
+          </div>
+          <div class="col-md-3 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services py-4 d-block text-center">
+              <div class="d-flex justify-content-center"><div class="icon"><span class="flaticon-purse"></span></div></div>
+              <div class="media-body p-2 mt-2">
+                <h3 class="heading mb-3">Making Money</h3>
+                <p>A small river named Duden flows by their place and supplies.</p>
+              </div>
+            </div>      
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    <section class="ftco-section ftco-properties">
+    	<div class="container">
+    		<div class="row justify-content-center mb-5 pb-3">
+          <div class="col-md-7 heading-section text-center ftco-animate">
+          	<span class="subheading">Recent Posts</span>
+            <h2 class="mb-4">Recent Properties</h2>
+          </div>
+        </div>
+    		<div class="row">
+    			<div class="col-md-12">
+    				<div class="properties-slider owl-carousel ftco-animate">
+    					<div class="item">
+		    				<div class="properties">
+		    					<a href="#" class="img d-flex justify-content-center align-items-center" style="background-image: url(images/properties-1.jpg);">
+		    						<div class="icon d-flex justify-content-center align-items-center">
+		    							<span class="icon-search2"></span>
+		    						</div>
+		    					</a>
+		    					<div class="text p-3">
+		    						<span class="status sale">Sale</span>
+		    						<div class="d-flex">
+		    							<div class="one">
+				    						<h3><a href="#">North Parchmore Street</a></h3>
+				    						<p>Apartment</p>
+			    						</div>
+			    						<div class="two">
+			    							<span class="price">$20,000</span>
+		    							</div>
+		    						</div>
+		    					</div>
+		    				</div>
+	    				</div>
+	    				<div class="item">
+		    				<div class="properties">
+		    					<a href="#" class="img d-flex justify-content-center align-items-center" style="background-image: url(images/properties-2.jpg);">
+		    						<div class="icon d-flex justify-content-center align-items-center">
+		    							<span class="icon-search2"></span>
+		    						</div>
+		    					</a>
+		    					<div class="text p-3">
+		    						<div class="d-flex">
+		    							<span class="status rent">Rent</span>
+		    							<div class="one">
+				    						<h3><a href="#">North Parchmore Street</a></h3>
+				    						<p>Apartment</p>
+			    						</div>
+			    						<div class="two">
+			    							<span class="price">$2,000 <small>/ month</small></span>
+		    							</div>
+		    						</div>
+		    					</div>
+		    				</div>
+	    				</div>
+	    				<div class="item">
+		    				<div class="properties">
+		    					<a href="#" class="img d-flex justify-content-center align-items-center" style="background-image: url(images/properties-3.jpg);">
+		    						<div class="icon d-flex justify-content-center align-items-center">
+		    							<span class="icon-search2"></span>
+		    						</div>
+		    					</a>
+		    					<div class="text p-3">
+		    						<span class="status sale">Sale</span>
+		    						<div class="d-flex">
+		    							<div class="one">
+				    						<h3><a href="#">North Parchmore Street</a></h3>
+				    						<p>Apartment</p>
+			    						</div>
+			    						<div class="two">
+			    							<span class="price">$20,000</span>
+		    							</div>
+		    						</div>
+		    					</div>
+		    				</div>
+	    				</div>
+	    				<div class="item">
+		    				<div class="properties">
+		    					<a href="#" class="img d-flex justify-content-center align-items-center" style="background-image: url(images/properties-4.jpg);">
+		    						<div class="icon d-flex justify-content-center align-items-center">
+		    							<span class="icon-search2"></span>
+		    						</div>
+		    					</a>
+		    					<div class="text p-3">
+		    						<span class="status sale">Sale</span>
+		    						<div class="d-flex">
+		    							<div class="one">
+				    						<h3><a href="#">North Parchmore Street</a></h3>
+				    						<p>Apartment</p>
+			    						</div>
+			    						<div class="two">
+			    							<span class="price">$20,000</span>
+		    							</div>
+		    						</div>
+		    					</div>
+		    				</div>
+	    				</div>
+	    				<div class="item">
+		    				<div class="properties">
+		    					<a href="#" class="img d-flex justify-content-center align-items-center" style="background-image: url(images/properties-5.jpg);">
+		    						<div class="icon d-flex justify-content-center align-items-center">
+		    							<span class="icon-search2"></span>
+		    						</div>
+		    					</a>
+		    					<div class="text p-3">
+		    						<span class="status rent">Rent</span>
+		    						<div class="d-flex">
+		    							<div class="one">
+				    						<h3><a href="#">North Parchmore Street</a></h3>
+				    						<p>Apartment</p>
+			    						</div>
+			    						<div class="two">
+			    							<span class="price">$900 <small>/ month</small></span>
+		    							</div>
+		    						</div>
+		    					</div>
+		    				</div>
+	    				</div>
+	    				<div class="item">
+		    				<div class="properties">
+		    					<a href="#" class="img d-flex justify-content-center align-items-center" style="background-image: url(images/properties-6.jpg);">
+		    						<div class="icon d-flex justify-content-center align-items-center">
+		    							<span class="icon-search2"></span>
+		    						</div>
+		    					</a>
+		    					<div class="text p-3">
+		    						<span class="status sale">Sale</span>
+		    						<div class="d-flex">
+		    							<div class="one">
+				    						<h3><a href="#">North Parchmore Street</a></h3>
+				    						<p>Apartment</p>
+			    						</div>
+			    						<div class="two">
+			    							<span class="price">$20,000</span>
+		    							</div>
+		    						</div>
+		    					</div>
+		    				</div>
+	    				</div>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+    </section>
+
+    <section class="ftco-section bg-light">
+    	<div class="container">
+				<div class="row justify-content-center mb-5 pb-3">
+          <div class="col-md-7 heading-section text-center ftco-animate">
+          	<span class="subheading">Special Offers</span>
+            <h2 class="mb-4">Most Recommended Properties</h2>
+          </div>
+        </div>    		
+    	</div>
+    	<div class="container-fluid">
+    		<div class="row">
+    			<div class="col-sm col-md-6 col-lg ftco-animate">
+    				<div class="properties">
+    					<a href="#" class="img img-2 d-flex justify-content-center align-items-center" style="background-image: url(images/properties-1.jpg);">
+    						<div class="icon d-flex justify-content-center align-items-center">
+    							<span class="icon-search2"></span>
+    						</div>
+    					</a>
+    					<div class="text p-3">
+    						<span class="status sale">Sale</span>
+    						<div class="d-flex">
+    							<div class="one">
+		    						<h3><a href="#">North Parchmore Street</a></h3>
+		    						<p>Apartment</p>
+	    						</div>
+	    						<div class="two">
+	    							<span class="price">$20,000</span>
+    							</div>
+    						</div>
+    						<p>Far far away, behind the word mountains, far from the countries</p>
+    						<hr>
+    						<p class="bottom-area d-flex">
+    							<span><i class="flaticon-selection"></i> 250sqft</span>
+    							<span class="ml-auto"><i class="flaticon-bathtub"></i> 3</span>
+    							<span><i class="flaticon-bed"></i> 4</span>
+    						</p>
+    					</div>
+    				</div>
+    			</div>
+    			<div class="col-sm col-md-6 col-lg ftco-animate">
+    				<div class="properties">
+    					<a href="#" class="img img-2 d-flex justify-content-center align-items-center" style="background-image: url(images/properties-2.jpg);">
+    						<div class="icon d-flex justify-content-center align-items-center">
+    							<span class="icon-search2"></span>
+    						</div>
+    					</a>
+    					<div class="text p-3">
+    						<span class="status sale">Sale</span>
+    						<div class="d-flex">
+    							<div class="one">
+		    						<h3><a href="#">North Parchmore Street</a></h3>
+		    						<p>Apartment</p>
+	    						</div>
+	    						<div class="two">
+	    							<span class="price">$20,000</span>
+    							</div>
+    						</div>
+    						<p>Far far away, behind the word mountains, far from the countries</p>
+    						<hr>
+    						<p class="bottom-area d-flex">
+    							<span><i class="flaticon-selection"></i> 250sqft</span>
+    							<span class="ml-auto"><i class="flaticon-bathtub"></i> 3</span>
+    							<span><i class="flaticon-bed"></i> 4</span>
+    						</p>
+    					</div>
+    				</div>
+    			</div>
+    			<div class="col-sm col-md-6 col-lg ftco-animate">
+    				<div class="properties">
+    					<a href="#" class="img img-2 d-flex justify-content-center align-items-center" style="background-image: url(images/properties-3.jpg);">
+    						<div class="icon d-flex justify-content-center align-items-center">
+    							<span class="icon-search2"></span>
+    						</div>
+    					</a>
+    					<div class="text p-3">
+    						<span class="status rent">Rent</span>
+    						<div class="d-flex">
+    							<div class="one">
+		    						<h3><a href="#">North Parchmore Street</a></h3>
+		    						<p>Apartment</p>
+	    						</div>
+	    						<div class="two">
+	    							<span class="price">$800 <small>/ month</small></span>
+    							</div>
+    						</div>
+    						<p>Far far away, behind the word mountains, far from the countries</p>
+    						<hr>
+    						<p class="bottom-area d-flex">
+    							<span><i class="flaticon-selection"></i> 250sqft</span>
+    							<span class="ml-auto"><i class="flaticon-bathtub"></i> 3</span>
+    							<span><i class="flaticon-bed"></i> 4</span>
+    						</p>
+    					</div>
+    				</div>
+    			</div>
+    			<div class="col-sm col-md-6 col-lg ftco-animate">
+    				<div class="properties">
+    					<a href="#" class="img img-2 d-flex justify-content-center align-items-center" style="background-image: url(images/properties-4.jpg);">
+    						<div class="icon d-flex justify-content-center align-items-center">
+    							<span class="icon-search2"></span>
+    						</div>
+    					</a>
+    					<div class="text p-3">
+    						<span class="status sale">Sale</span>
+    						<div class="d-flex">
+    							<div class="one">
+		    						<h3><a href="#">North Parchmore Street</a></h3>
+		    						<p>Apartment</p>
+	    						</div>
+	    						<div class="two">
+	    							<span class="price">$20,000</span>
+    							</div>
+    						</div>
+    						<p>Far far away, behind the word mountains, far from the countries</p>
+    						<hr>
+    						<p class="bottom-area d-flex">
+    							<span><i class="flaticon-selection"></i> 250sqft</span>
+    							<span class="ml-auto"><i class="flaticon-bathtub"></i> 3</span>
+    							<span><i class="flaticon-bed"></i> 4</span>
+    						</p>
+    					</div>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+    </section>
+
+
+		<?php include 'includes\footer.html';?>
+    
+  
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/bootstrap-datepicker.js"></script>
+  <script src="js/jquery.timepicker.min.js"></script>
+  <script src="js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="js/google-map.js"></script>
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
